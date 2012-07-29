da9c573cb6d8f12f3e172bd2b1382a68888c2822
Dave <djm@cs.hmc.edu>
2012-07-29 14:26:20 -0700

    More progress on a basic example
    You can now see more of how we manage things here.  Added in some
    graphics positioning and some key-specific behavior (moving things
    around on the screen).  Also added (at a lark) framerate limiting code,
    which is amazingly powerful and useful as it turns out.

--- example.py
+++ example.py
@@ -12,6 +12,10 @@ import sys
 
 pygame.init()
 
+# generate a frame-rate managing clock
+
+fpsClock = pygame.time.Clock()
+
 # generate a screen-visible service
 
 surfScreen = pygame.display.set_mode((640, 480))
@@ -31,9 +35,14 @@ colorWhite = pygame.Color(255, 255, 255)
 lColor = [colorRed, colorGreen, colorBlue, colorPurple, colorBlack, colorWhite]
 iColor = 0
 
-# initialize to red (why not!)
+# load up an image
 
-surfScreen.fill(colorRed)
+surfStars = pygame.image.load('stars.png')
+posStars = (320, 240)
+
+# initialize to first color
+
+surfScreen.fill(lColor[0])
 
 # run an event loop
 
@@ -45,9 +54,31 @@ while True:
 			sys.exit(0)
 
 		elif event.type == pygame.KEYDOWN:
-			iColor = (iColor + 1) % len(lColor)
-			surfScreen.fill(lColor[iColor])
+			if event.key == pygame.K_LEFT:
+				posStars = ((posStars[0] + 637) % 640, posStars[1])
+			elif event.key == pygame.K_RIGHT:
+				posStars = ((posStars[0] + 643) % 640, posStars[1])
+			elif event.key == pygame.K_UP:
+				posStars = (posStars[0], (posStars[1] + 477) % 480)
+			elif event.key == pygame.K_DOWN:
+				posStars = (posStars[0], (posStars[1] + 483) % 480)
+			else:
+				iColor = (iColor + 1) % len(lColor)
+
+	# redraw the things we want drawn
+
+	surfScreen.fill(lColor[iColor])
+	surfScreen.blit(surfStars, posStars)
 			
 	# refresh the screen -- very important to do at the end of the loop!
 
 	pygame.display.update()
+
+	# make sure we don't redraw more than 30 times a second
+
+	# NOTE (dave) this is actually extremely important -- without this,
+	#  the update loop will happily consume all of the available CPU
+	#  resources, and end up feeling *worse* response-wise than what
+	#  we get this way.  absolutely amazing...
+
+	fpsClock.tick(30)
