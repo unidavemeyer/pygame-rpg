a7831d979cfd1091574826431890bcda00319b22
David Meyer <djm@cs.hmc.edu>
2013-07-30 17:57:34 -0700

    Joystick code updates
    Now have better support, with names, for various buttons and for the
    sundry axis (analog) data.  Compiles fine, although I have not
    made any attempt to use the class in question yet.

--- Joystick.py
+++ Joystick.py
@@ -20,6 +20,51 @@ class Joystick:
 	"""Represents a single joystick device connected to the system; provies an"""
 	""" abstract and cooked representation of the underlying pygame joystick data."""
 
+	# NOTE (davidm) here is experimentally-derived data from using a
+	#  ps3 controller and examining what happened with various input
+	#  channels:
+
+	# PIA = PS3 Index for Axis
+
+	PIA_L_LR	= 0		# axis 0 = left stick, side-to-side, -1 = left, 1.0 = right
+	PIA_L_UD	= 1		# axis 1 = left stick, up-and-down, -1 = up, 1.0 = down
+	PIA_R_LR	= 2		# axis 2 = right stick, side-to-side, -1 = left, 1.0 = right
+	PIA_R_UD	= 3		# axis 3 = right stick, up-and-down, -1 = up, 1.0 = down
+	PIA_PAD_U	= 8		# axis 8 = dpad up, analog pressure, -1 = up, 1 = down
+	PIA_PAD_L	= 9		# axis 9 = dpad left, analog pressure, -1 = up, 1 = down
+	PIA_PAD_D	= 10	# axis 10 = dpad down, analog pressure, -1 = up, 1 = down
+	PIA_L2		= 12	# axis 12 = l2, analog pressure, -1 = up, 1 = down
+	PIA_R2		= 13	# axis 13 = r2, analog pressure, -1 = up, 1 = down
+	PIA_L1		= 14	# axis 14 = l1, analog pressure, -1 = up, 1 = down
+	PIA_R1		= 15	# axis 15 = r1, analog pressure, -1 = up, 1 = down
+	PIA_TRI		= 16	# axis 16 = triangle, analog pressure, -1 = up, 1 = down
+	PIA_CIR		= 17	# axis 17 = circle, analog pressure, -1 = up, 1 = down
+	PIA_X		= 18	# axis 18 = x, analog pressure, -1 = up, 1 = down
+	PIA_SQR		= 19	# axis 19 = square, analog pressure, -1 = up, 1 = down
+	PIA_ACL_LR	= 23	# axis 23 = left/right accel, neg = right down, pos = left down
+	PIA_ACL_FB	= 24	# axis 24 = fore/back accel, neg = fore down, pos = back down
+	PIA_ACL_UD	= 25	# axis 25 = up/down accel, neg = down down, pos = up down
+
+	# PIB = PS3 Index for Button
+
+	PIB_SEL		= 0		# 0 = select
+	PIB_L3		= 1		# 1 = left stick
+	PIB_R3		= 2		# 2 = right stick
+	PIB_STR		= 3		# 3 = start
+	PIB_PAD_U	= 4		# 4 = dpad up
+	PIB_PAD_R	= 5		# 5 = dpad right
+	PIB_PAD_D	= 6		# 6 = dpad down
+	PIB_PAD_L	= 7		# 7 = dpad left
+	PIB_L2		= 8		# 8 = l2 (?)
+	PIB_R2		= 9		# 9 = r2 (?)
+	PIB_L1		= 10	# 10 = l1 (?)
+	PIB_R1		= 11	# 11 = r1 (?)
+	PIB_TRI		= 12	# 12 = triangle
+	PIB_CIR		= 13	# 13 = circle
+	PIB_X		= 14	# 14 = x
+	PIB_SQR		= 15	# 15 = square
+	PIB_PS		= 16	# 16 = ps
+
 	def __init__(self, id):
 		self.joy = pygame.joystick.Joystick(id)
 		self.joy.init()
@@ -31,74 +76,37 @@ class Joystick:
 		self.lAxis = [0 for x in range(self.joy.get_numaxes())]
 		self.lBtn = [0 for x in range(self.joy.get_numbuttons())]
 
-		# BB (davidm) assumes ps3 controller
+		# BB (davidm) pick a different set of mappings for non-ps3 controllers
 
-		# NOTE (davidm) here is experimentally-derived data from using a
-		#  ps3 controller and examining what happened with various input
-		#  channels:
-
-		# axis 0 = left stick, side-to-side, -1 = left, 1.0 = right
-		# axis 1 = left stick, up-and-down, -1 = up, 1.0 = down
-		# axis 2 = right stick, side-to-side, -1 = left, 1.0 = right
-		# axis 3 = right stick, up-and-down, -1 = up, 1.0 = down
-		# axis 4 = no data
-		# axis 5 = no data
-		# axis 6 = no data
-		# axis 7 = no data
-		# axis 8 = dpad up, analog pressure, -1 = up, 1 = down
-		# axis 9 = dpad left, analog pressure, -1 = up, 1 = down
-		# axis 10 = dpad down, analog pressure, -1 = up, 1 = down
-		# axis 11 = no data
-		# axis 12 = l2, analog pressure, -1 = up, 1 = down
-		# axis 13 = r2, analog pressure, -1 = up, 1 = down
-		# axis 14 = l1, analog pressure, -1 = up, 1 = down
-		# axis 15 = r1, analog pressure, -1 = up, 1 = down
-		# axis 16 = triangle, analog pressure, -1 = up, 1 = down
-		# axis 17 = circle, analog pressure, -1 = up, 1 = down
-		# axis 18 = x, analog pressure, -1 = up, 1 = down
-		# axis 19 = square, analog pressure, -1 = up, 1 = down
-		# axis 20 = no data
-		# axis 21 = no data
-		# axis 22 = no data
-		# axis 23 = left/right accel, neg = right down, pos = left down
-		# axis 24 = fore/back accel, neg = fore down, pos = back down
-		# axis 25 = up/down accel, neg = down down, pos = up down
-		# axis 26 = no data
-
-		# 0 = select
-		# 1 = left stick
-		# 2 = right stick
-		# 3 = start
-		# 4 = dpad up
-		# 5 = dpad right
-		# 6 = dpad down
-		# 7 = dpad left
-		# 8 = l2 (?)
-		# 9 = r2 (?)
-		# 10 = l1 (?)
-		# 11 = r1 (?)
-		# 12 = triangle
-		# 13 = circle
-		# 14 = x
-		# 15 = square
-		# 16 = ps
 
 	def ThumbLeftLR(self):
 		"""-1.0 (left) to 1.0 (right) value for the position of the left stick"""
 
-		return self.lAxis[0]
+		return self.lAxis[PIA_L_LR]
 
 	def ThumbLeftUD(self):
 		"""-1.0 (up) to 1.0 (down) value for the position of the left stick"""
 
-		return self.lAxis[1]
+		return self.lAxis[PIA_L_UD]
 
 	def ThumbRightLR(self):
 		"""-1.0 (left) to 1.0 (right) value for the position of the right stick"""
 
-		return self.lAxis[2]
+		return self.lAxis[PIA_R_LR]
 
 	def ThumbRightUD(self):
 		"""-1.0 (up) to 1.0 (down) value for the position of the right stick"""
 
-		return self.lAxis[3]
+		return self.lAxis[PIA_R_UD]
+
+	def FIsBtnDown(self, btn):
+		mpBtnIbtn = [
+				PIB_PAD_U,	# BTN_NavUp (= pad up)
+				PIB_PAD_D,	# BTN_NavDown (= pad down)
+				PIB_PAD_L,	# BTN_NavLeft (= pad left)
+				PIB_PAD_R,	# BTN_NavRight (= pad right)
+				PIB_X,		# BTN_Ok (= x)
+				PIB_CIR,	# BTN_Cancel = 5
+			]
+
+		return self.lBtn[mpBtnIbtn[btn]]
