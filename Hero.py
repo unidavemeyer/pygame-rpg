45bb9e25d5f82e3f270d23d7098048a4ebf91f36
Dave Meyer <unidavemeyer@gmail.com>
2022-11-15 19:58:50 -0800

    HP bar display, in progress
    Added basic HP bar to the hero. Basically works, although could use some
    sprucing up to make the colors more interesting as damage is taken. Also
    fixed several bugs where things didn't work, and improved the behavior
    of the Episode menu (which was confusing -- it left itself pushed after
    selecting a world, which was kind of goofy for subsequent uses of the
    menu).

--- Hero.py
+++ Hero.py
@@ -6,6 +6,44 @@ import pygame
 import Vec
 import Weapon
 
+
+
+def RenderHpBar(surfScreen, pos, hpCur, hpMax):
+	"""Renders a hit point bar to the screen based on the position for an entity and its current/max hitpoint
+	values"""
+
+	# BB (davidm) seems like we can/should commonize this to be somewhere else, yes?
+
+	# Calculate fraction of full health that hpCur represents
+
+	uHp = min(max(hpCur / hpMax, 0.0), 1.0)
+	wHp = uHp * 32
+	wBack = 32 - wHp
+
+	# Calculate hp and background bar sizes
+
+	s_dYHpOffset = 33
+	s_dYHp = 3
+
+	yHpTop = pos.y + s_dYHpOffset
+
+	rectHp = pygame.Rect(pos.x, yHpTop, wHp, s_dYHp)
+	rectHpBack = pygame.Rect(pos.x + wHp, yHpTop, wBack, s_dYHp)
+
+	# Calculate color for hp bar
+
+	rgbHp = (128, 255, 128)	 # BB (davidm) make this interesting - interpolate based on uHp, green -> red
+	rgbHpBack = (0, 0, 0)
+
+	# Draw the bar (both parts, as necessary)
+
+	surfScreen.fill(rgbHp, rectHp)
+
+	if wBack > 0:
+		surfScreen.fill(rgbHpBack, rectHpBack)
+
+
+
 class Hero:
 	"""The game contains one or more Hero instances, which handles input, rendering, and"""
 	""" positioning in worldmap mode.  They also handle inventory functions, hit points,"""
@@ -15,7 +53,7 @@ class Hero:
 		Game.game.AddUpdate(self, 20)	# relatively early update
 		Game.game.AddRender(self, 90)	# relatively late render (more on top)
 		Game.game.AddHandler(self, 20)	# relatively early event handler
-		self.fIsMajicAttackActive = False
+		self.fIsMagicAttackActive = False
 		
 		# Joystick tracking
 
@@ -185,7 +223,7 @@ class Hero:
 			return False
 
 		# BB (dave) handle 'i' key to go to inventory
-        
+		
 		if event.type == pygame.KEYDOWN:
 			keystate = self.mpKeyState.get(event.key)
 			if keystate:
@@ -203,6 +241,8 @@ class Hero:
 		if Game.game.Mode() == Game.Mode.WORLDMAP:
 			# BB (dave) very basic positioning here -- can flow off sides, no collision, etc.
 			surfScreen.blit(self.surf, (int(self.pos.x), int(self.pos.y)))
+
+			RenderHpBar(surfScreen, self.pos, self.hpCur, self.hpMax)
 			
 		elif Game.game.Mode() == Game.Mode.COMBAT:
 			# BB (davidm) draw the hero
@@ -289,15 +329,15 @@ class Hero:
 		vY = vMax * uUd
 
 		return Vec.Vec(vX, vY)
-        
+		
 	def UpdateAttackMagic(self):
 		
-		keyeyStateAttack = self.mpKeyState.get(pygame.K_e)
-		if KeyStateAttack.FIsPressed() and not self.fIsMagicAttackActive:
+		keyStateAttack = self.mpKeyState.get(pygame.K_e)
+		if keyStateAttack.FIsPressed() and not self.fIsMagicAttackActive:
 			for npc in Game.game.LNpc():
 				npc.hpCur -= 10  #BB Npc ondamage maybe? - Z.A.C.
 			self.fIsMagicAttackActive = True
-		if not KeyStateAttack.FIsPressed() and self.fIsMagicAttackActive:
+		if not keyStateAttack.FIsPressed() and self.fIsMagicAttackActive:
 			self.fIsMagicAttackActive = False
 
 	def VTargetComputeKeyboard(self):
